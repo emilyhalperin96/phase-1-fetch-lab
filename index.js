07a0d02e91c4663a1f044f0cd525558e33a117e2
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-17 08:54:48 -0500

    done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(books => renderBooks(books));
 }
+//call renderBooks function and pass in the JSONified data as the argument
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +15,4 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks(); })
